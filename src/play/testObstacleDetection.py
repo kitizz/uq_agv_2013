889e934fc29ece8babe40ba4b70b7d1346a087e7
Kit Ham <kitizz.devside@gmail.com>
2013-11-19 02:02:10 +1000

    Plane vs Object Detection working
    I haven't uploaded the dataset I used for testing this code. It's too big.
    This was adapted from PCL's SAC model stuff. This is just working for
    plane detection.
    
    This keeps up when the ground plane is nicely visible by feeding the
    previous known location into the next frame.

--- src/play/testObstacleDetection.py
+++ src/play/testObstacleDetection.py
@@ -1,6 +1,9 @@
 
-
+import cv2
 import numpy as np
+
+import SACModelPlane
+import RRansac
 
 class ObstacleDetector:
 
@@ -8,6 +11,8 @@ class ObstacleDetector:
 		self.image = np.zeros(size)
 		self.depth = np.zeros(size)
 		self.points = np.zeros(size)
+		self.coeff = np.array([0.,0.,1.,-15.])
+		self.coeffRating = 1.
 
 	def updateImage(self, image_msg):
 		self.image = image_msg.image
@@ -19,9 +24,64 @@ class ObstacleDetector:
 		self.points = point_msg.points
 		self.update()
 
+	@staticmethod
+	def goodCoeff(coeff):
+		maxTheta = np.deg2rad(45)
+		norm = coeff[0:3]
+		des = np.array([0.,1.,0.])
+		theta = np.arccos( np.dot(norm, des) )
+		return theta <= maxTheta
+
 	def update(self):
-		
-		pass
+		# This is where the magic happens
+		depth = np.nan_to_num(self.points[...,...,2])
+		depth /= 50.
+		cv2.imshow('Depth', depth)
+
+		model = SACModelPlane.SACModelPlane(self.points)
+
+		startCoeff = None
+		if self.coeffRating > 0.3: # 30 percent coverage
+			success, initialFit = model.selectWithinDistance(self.coeff, 1.)
+			if len(initialFit) > 10: startCoeff = self.coeff
+			print 'Initial Fit:', len(initialFit)
+
+		ran = RRansac.RRansac(model, 1.5)
+		ran.maxIterations = 200
+		ran.fractionPretest = 0.01
+
+		# cv2.waitKey()
+
+		success, self.coeff, inlierInd, select = ran.computeModel(startCoeff, constraint=self.goodCoeff)
+		if success:
+			N = float(len(inlierInd))
+			self.coeffRating = N/len(model.indices)
+
+			# Ensure that the plane is always facing "up" (Y-axis negative)
+			if np.sign(self.coeff[1]) < 0:
+				self.coeff *= -1
+
+			print "Inliers:", N
+			print "Coeffs:", self.coeff
+			# Convert the image to colour so we can draw on it
+			imageVis = cv2.cvtColor(self.image, cv2.COLOR_GRAY2RGB)
+			imCoord = model.ind2coord(inlierInd)
+			# imageVis[imCoord][0:2] = 0. # Highlight plane in red
+			imageVis[imCoord] = [0, 0, 255]
+
+			# From points not defined as the plane, we define object points as
+			# points that are above the ground plane (negative side)
+			outlierInd = np.setdiff1d(model.indices, inlierInd)
+			success, dist = model.getDistancesToModel(self.coeff, \
+				subset=outlierInd, signed=True)
+			if success:
+				objectInd = outlierInd[dist<0]
+				objectCoord = model.ind2coord(objectInd)
+				# imageVis[objectCoord][1:] = 0.
+				imageVis[objectCoord] = [255, 0, 0]
+
+			cv2.imshow('Plane', imageVis)
+			cv2.waitKey(10)
 
 class PointsMsg:
 	def __init__(self,stamp,cloud):
@@ -86,14 +146,25 @@ def main():
 	indImg = 0
 	indPnt = 0
 	obDetect = ObstacleDetector((240,320))
+	lastImage = None
+	lastPoints = None
 	while indImg < nImages and indPnt < nPoints:
-		if indPnt >= nPoints or (indImg < nImages and images[indImg].stamp < points[indPnt].stamp):
+		if indPnt >= nPoints or \
+		(indImg < nImages and images[indImg].stamp < points[indPnt].stamp):
 			obDetect.updateImage(images[indImg])
+			lastImage = images[indImg]
 			indImg += 1
+			# cv2.imshow('Loading', lastImage.image)
+			# cv2.waitKey(30)
 
 		else:
-			obDetect.updateImage(points[indPnt])
+			obDetect.updatePoints(points[indPnt])
+			lastPoints = points[indPnt]
 			indPnt += 1
+
+	# Let's give it a spin...
+	# obDetect.updateImage(lastImage)
+	# obDetect.updatePoints(lastPoints)
 
 
 if __name__ == '__main__':
