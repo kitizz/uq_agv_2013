b6aa683a07db08ccf019316b0eed415825f2bed3
Kit Ham <kitizz.devside@gmail.com>
2013-11-25 17:39:14 +1000

    Making MotorControl more robust with serial selection

--- src/MotorControl.py
+++ src/MotorControl.py
@@ -3,9 +3,27 @@ import serial
 
 class MotorControl:
 
-	def __init__(self, port, baud=9600):
-		self.ser = serial.Serial(port, baud, timeout=2)
-		self.ser.open()
+	def __init__(self, prefix, ports=[], baud=9600):
+		'''
+		Connect the motor control at serial port defined as:
+		prefix: The port prefix, e.g. /dev/ttyUSB, /dev/ttyACM, etc.
+		ports: A list of port numbers (as a suffix) to attempt to connect to.
+		baud: Baud...
+		'''
+		if not ports:
+			ports = range(3)
+
+		success = False
+		for port in ports:
+			dev = prefix + str(port)
+			try:
+				self.ser = serial.Serial(dev, baud, timeout=2)
+			except:
+				print "Failed to open:", dev, "Attempting next port..."
+			else:
+				print "Succesfully opened:", dev
+				break
+
 
 	def setLeftMotor(self, v):
 		''' setLeftMotor(velocity)
@@ -17,18 +35,18 @@ class MotorControl:
 		# 90 = no speed
 		# 150 = full speed forwards
 		vel = int( float(v)/60 + 90 )
-		self.ser.write('L' + str(v))
+		self.ser.write('L' + str(vel))
+		self.ser.flush()
 
 	def setRightMotor(self, v):
 		''' setLeftMotor(velocity)
 		Takes a velocity valued between -100 and 100 to send to the left motor.
 		'''
 		vel = int (float(v)/60 + 90 )
-		self.ser.write('R' + str(v))
+		self.ser.write('R' + str(vel))
+		self.ser.flush()
 
 	def setMotor(self, vL, vR):
-		for i in range(10):
-			self.ser.write('L' + str(vL))
-			self.ser.write('R' + str(vR))
+		self.setLeftMotor(vL)
+		self.setRightMotor(vR)
 
-	
